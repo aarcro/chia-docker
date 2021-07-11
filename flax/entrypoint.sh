5c0ec404e9f9ed8666046270aa3dd81317ef8286
Aaron McMillin <github@aaron.mcmillinclan.org>
2021-07-11 13:44:43 -0400

    flax

---
+++ flax/entrypoint.sh
@@ -0,0 +1,61 @@
+if [[ -n "${TZ}" ]]; then
+  echo "Setting timezone to ${TZ}"
+  sudo ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ | sudo tee /etc/timezone
+fi
+
+. ./activate
+
+flax init
+
+if [[ ${keys} == "generate" ]]; then
+  echo "to use your own keys pass them as a text file -v /path/to/keyfile:/path/in/container and -e keys=\"/path/in/container\""
+  flax keys generate
+elif [[ ${keys} == "prompt" ]]; then
+  echo "Input your keys"
+  flax keys add
+elif [[ ${keys} == "copy" ]]; then
+  if [[ -z ${ca} ]]; then
+    echo "A path to a copy of the farmer peer's ssl/ca required."
+	exit
+  else
+  flax init -c ${ca}
+  fi
+else
+  chia keys add -f ${keys}
+fi
+
+for p in ${plots_dir//:/ }; do
+    mkdir -p ${p}
+    if [[ ! "$(ls -A $p)" ]]; then
+        echo "Plots directory '${p}' appears to be empty, try mounting a plot directory with the docker -v command"
+    fi
+    flax plots add -d ${p}
+done
+
+sed -i 's/localhost/127.0.0.1/g' ~/.flax/mainnet/config/config.yaml
+
+flax configure --set-log-level ${log_level}
+
+if [[ ${farmer} == 'true' ]]; then
+  flax start farmer-only
+elif [[ ${harvester} == 'true' ]]; then
+  if [[ -z ${farmer_address} || -z ${farmer_port} || -z ${ca} ]]; then
+    echo "A farmer peer address, port, and ca path are required."
+    exit
+  else
+    flax configure --set-farmer-peer ${farmer_address}:${farmer_port}
+    flax start harvester
+  fi
+else
+  flax start farmer
+fi
+
+if [[ ${testnet} == "true" ]]; then
+  if [[ -z $full_node_port || $full_node_port == "null" ]]; then
+    flax configure --set-fullnode-port 58444
+  else
+    flax configure --set-fullnode-port ${var.full_node_port}
+  fi
+fi
+
+tail -F ~/.flax/mainnet/log/debug.log
