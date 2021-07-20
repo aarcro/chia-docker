535925a0d5a78ee56254337878272f24ce177e54
Aaron McMillin <github@aaron.mcmillinclan.org>
2021-07-20 09:55:41 -0400

    MASS stuff, maybe

---
+++ mass/entrypoint.sh
@@ -0,0 +1,18 @@
+if [[ -n "${TZ}" ]]; then
+  echo "Setting timezone to ${TZ}"
+  sudo ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ | sudo tee /etc/timezone
+fi
+
+# What do?
+for p in ${plots_dir//:/ }; do
+    mkdir -p ${p}
+    if [[ ! "$(ls -A $p)" ]]; then
+        echo "Plots directory '${p}' appears to be empty, try mounting a plot directory with the docker -v command"
+    fi
+    echo "Plots are here: ${p}"
+done
+
+# Wait forever
+echo "Going to sleep"
+tail -f /dev/null
+echo "Why ded?"
---
+++ mass/readme.md
@@ -0,0 +1,10 @@
+# Mass miner
+
+Go build: https://github.com/massnetorg/MassNet-miner.git
+And copy the /bin/\* here
+
+If you don't want to use UID 1000, update some stuff
+
+
+This is all untested. DONT expose 9685 9686, to the internet, those are the ports for cli to talk to the miner.
+(You wouldn't put your chia daemon on the internet)
