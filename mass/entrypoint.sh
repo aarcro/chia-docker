2e95e907fe648bcb73e14d80e4559e02e49c339f
Aaron McMillin <github@aaron.mcmillinclan.org>
2021-08-26 11:06:33 -0400

    pool/fractal config

--- mass/entrypoint.sh
+++ mass/entrypoint.sh
@@ -3,14 +3,26 @@ if [[ -n "${TZ}" ]]; then
   sudo ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ | sudo tee /etc/timezone
 fi
 
-if [[ -n ${payout_address} ]]; then
-  envsubst < miner-config.json > mc.json
-  mv mc.json miner-config.json
+export PATH=~/bin:${PATH}
+
+if [[ ${mode} == "fractal" ]]; then
+    # Fractal miner
+    massminer -C fractal-config.json fractal -s ${miner_ip_port} &
+else
+    # Full node
+    if [[ -n ${payout_address} ]]; then
+      envsubst < miner-config.json > mc.json
+      mv mc.json miner-config.json
+    fi
+
+    masswallet -C wallet-config.json 2>&1 > /dev/null &
+    if [[ ${mode} == "collector" ]]; then
+        massminer -C miner-config.json m2 -p 0.0.0.0:9690 &
+    else
+        massminer -C miner-config.json m2 &
+    fi
 fi
 
-export PATH=~/bin:${PATH}
-masswallet -C wallet-config.json 2>&1 > /dev/null &
-massminer -C miner-config.json m2 &
 
 # Wait forever
 echo "Going to sleep"
