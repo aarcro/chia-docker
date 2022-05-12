2939d6d09bc4db57f102165135d06a2da975efe2
Aaron McMillin <github@aaron.mcmillinclan.org>
2022-05-11 23:38:27 -0400

    fix stai, add petroleum

--- petroleum/entrypoint.sh
+++ petroleum/entrypoint.sh
@@ -5,14 +5,14 @@ fi
 
 . ./activate
 
-stai init
+petroleum init
 
 if [[ ${keys} == "generate" ]]; then
   echo "to use your own keys pass them as a text file -v /path/to/keyfile:/path/in/container and -e keys=\"/path/in/container\""
-  stai keys generate
+  petroleum keys generate
 elif [[ ${keys} == "prompt" ]]; then
   echo "Input your keys"
-  stai keys add
+  petroleum keys add
 elif [[ ${keys} == "none" ]]; then
   echo "using keychain for keys"
 elif [[ ${keys} == "copy" ]]; then
@@ -20,10 +20,10 @@ elif [[ ${keys} == "copy" ]]; then
     echo "A path to a copy of the farmer peer's ssl/ca required."
 	exit
   else
-  stai init -c ${ca}
+  petroleum init -c ${ca}
   fi
 else
-  stai keys add -f ${keys}
+  petroleum keys add -f ${keys}
 fi
 
 for p in ${plots_dir//:/ }; do
@@ -31,32 +31,32 @@ for p in ${plots_dir//:/ }; do
     if [[ ! "$(ls -A $p)" ]]; then
         echo "Plots directory '${p}' appears to be empty, try mounting a plot directory with the docker -v command"
     fi
-    stai plots add -d ${p}
+    petroleum plots add -d ${p}
 done
 
-sed -i 's/localhost/127.0.0.1/g' ~/.stai/mainnet/config/config.yaml
+sed -i 's/localhost/127.0.0.1/g' ~/.petroleum/mainnet/config/config.yaml
 
-stai configure --set-log-level ${log_level}
+petroleum configure --set-log-level ${log_level}
 
 if [[ ${farmer} == 'true' ]]; then
-  stai start farmer-only
+  petroleum start farmer-only
 elif [[ ${harvester} == 'true' ]]; then
   if [[ -z ${farmer_address} || -z ${farmer_port} || -z ${ca} ]]; then
     echo "A farmer peer address, port, and ca path are required."
     exit
   else
-    stai configure --set-farmer-peer ${farmer_address}:${farmer_port}
-    stai start harvester
+    petroleum configure --set-farmer-peer ${farmer_address}:${farmer_port}
+    petroleum start harvester
   fi
 else
-  stai start farmer
+  petroleum start farmer
 fi
 
 if [[ ${testnet} == "true" ]]; then
   if [[ -z $full_node_port || $full_node_port == "null" ]]; then
-    stai configure --set-fullnode-port 58444
+    petroleum configure --set-fullnode-port 58444
   else
-    stai configure --set-fullnode-port ${var.full_node_port}
+    petroleum configure --set-fullnode-port ${var.full_node_port}
   fi
 fi
 
---
+++ petroleum/readme.md
@@ -0,0 +1,9 @@
+# petroleum fork
+
+Old busted: docker attach to the full node and paste/type 24 words at startup (Or do your own config thing)
+
+New hottness: docker volume for ~/.local => `docker-compose exec fullnode keys add` once, saved forever.
+
+Create all the ~/.petroleum directories first, or docker will create them as root before dropping privs
+
+If you don't want to use UID 1000, update some stuff
